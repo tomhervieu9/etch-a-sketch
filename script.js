5705aa583edce465eda5f2e62686c418b2c416ad
Thomas Hervieu <tom.hervieu@gmail.com>
2022-11-24 14:56:15 -0500

    Add hover styling and polish layout of the grid

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function createGrid() {
-    const container = document.querySelector('body div');
+    const container = document.querySelector('.container');
 
-    for (let i = 0; i < 15; i++) {
+    const numSquares = prompt('How many squares would you like on each side?', 4);
+
+    container.style.gridTemplateColumns = `repeat(${numSquares}, 1fr)`;
+
+    for (let i = 0; i < numSquares**2; i++) {
         const div = document.createElement('div');
         div.classList.add('square');
+        div.addEventListener('mouseover', (e) => div.classList.add('hover'), false);
+        div.addEventListener('mouseout', (e) => {div.classList.remove('hover')}, false);
         container.appendChild(div);
     }
+
 }
 
 createGrid();

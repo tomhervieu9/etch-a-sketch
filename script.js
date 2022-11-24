d3189ccd06ac30d5912d55f4b6f38d27c04a3399
Thomas Hervieu <tom.hervieu@gmail.com>
2022-11-24 15:40:38 -0500

    Re-factor code and add button to change dimension

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function createGrid() {
-    const container = document.querySelector('.container');
+const container = document.querySelector('.container');
+const button = document.querySelector('button');
+button.addEventListener('click', changeDimension, false);
+changeDimension();
 
-    const numSquares = prompt('How many squares would you like on each side?', 4);
+function changeDimension() {
+    let numSquares = prompt('How many squares would you like on each side? (Max: 100)');
 
     container.style.gridTemplateColumns = `repeat(${numSquares}, 1fr)`;
+
+    removeAllChildNodes();
 
     for (let i = 0; i < numSquares**2; i++) {
         const div = document.createElement('div');
@@ -12,7 +17,10 @@ function createGrid() {
         div.addEventListener('mouseout', (e) => {div.classList.remove('hover')}, false);
         container.appendChild(div);
     }
-
 }
 
-createGrid();
+function removeAllChildNodes() {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
